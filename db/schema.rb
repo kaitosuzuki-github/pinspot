992fed59e2cea774596fc4255e7b1859f8c10f7b
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-04-06 17:34:44 +0900

    feat: #34 不要なカラムのため、postsテーブルからimage_urlカラムを削除する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_080912) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_082853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_080912) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "image_url"
     t.text "description"
     t.string "location"
     t.datetime "created_at", null: false
