5247fbd0083ca9f358d003a4510b6025e77b05b1
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-04-12 15:42:59 +0900

    feat: #51 postsテーブルにlatitudeカラム、longitudeカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_082853) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_063354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_082853) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
