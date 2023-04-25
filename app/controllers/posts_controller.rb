c98ff31c51cf04364631a9ef539f145b40d5aa47
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-04-25 13:29:06 +0900

    feat: #86 renderで@postを渡せていなかったため、postを@postに変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,14 +12,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    post.user_id = current_user.id
-    if post.save
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
       flash[:notice] = "登録しました"
       redirect_to new_post_path
     else
       flash.now[:notice] = "登録できませんでした"
-      render new_post_path
+      render :new
     end
   end
 
@@ -28,12 +28,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    if post.update(post_params)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
       flash[:notice] = "変更しました"
       redirect_to post_path
     else
-      render edit_post_path
+      flash.now[:notice] = "変更できませんでした"
+      render :edit
     end
   end
 
