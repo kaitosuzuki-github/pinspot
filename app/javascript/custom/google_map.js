5dd23c15cb42f01e74489f5fbbbf2ecd8bf0e163
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-01 21:42:33 +0900

    feat: #106 投稿機能のビューでgoolemapのマーカーが複数表示されないよう変更

--- app/javascript/custom/google_map.js
+++ app/javascript/custom/google_map.js
@@ -1,67 +1,65 @@
 document.addEventListener(
   "turbo:load",
   function () {
-    let geocoder;
-    let map;
     const mapDisplay = document.getElementById("map");
     const mapSearch = document.getElementById("mapSearch");
+    let map;
+    let marker;
     if (mapDisplay != null) {
-      map = initMap(mapDisplay, map);
+      initMap(mapDisplay);
       if (mapSearch != null) {
-        geocoder = new google.maps.Geocoder();
-        mapSearch.addEventListener(
-          "click",
-          { map: map, geocoder: geocoder, handleEvent: codeAddress },
-          false
-        );
+        mapSearch.addEventListener("click", codeAddress, false);
       }
+    }
+
+    function initMap(mapDisplay) {
+      const lat = mapDisplay.dataset.latitude;
+      const lng = mapDisplay.dataset.longitude;
+      if (lat == undefined && lng == undefined) {
+        map = new google.maps.Map(mapDisplay, {
+          center: { lat: 36.204824, lng: 138.252924 },
+          zoom: 4,
+        });
+        marker = null;
+      } else {
+        const floatLat = parseFloat(lat);
+        const floatLng = parseFloat(lng);
+        map = new google.maps.Map(mapDisplay, {
+          center: { lat: floatLat, lng: floatLng },
+          zoom: 12,
+        });
+        marker = new google.maps.Marker({
+          map: map,
+          position: { lat: floatLat, lng: floatLng },
+        });
+      }
+    }
+
+    function codeAddress() {
+      let geocoder = new google.maps.Geocoder();
+      let lat = document.getElementById("lat");
+      let lng = document.getElementById("lng");
+
+      const inputAddress = document.getElementById("address").value;
+      geocoder.geocode({ address: inputAddress }, function (results, status) {
+        if (status == "OK") {
+          const location = results[0].geometry.location;
+          map.setCenter(location);
+          if (marker != null) {
+            marker.setMap(null);
+          }
+          marker = null;
+          marker = new google.maps.Marker({
+            map: map,
+            position: location,
+          });
+          lat.value = location.lat();
+          lng.value = location.lng();
+        } else {
+          alert("該当する結果がありませんでした：" + status);
+        }
+      });
     }
   },
   false
 );
-
-function initMap(mapDisplay, map) {
-  if (
-    mapDisplay.dataset.latitude == undefined &&
-    mapDisplay.dataset.longitude == undefined
-  ) {
-    map = new google.maps.Map(mapDisplay, {
-      center: { lat: 36.204824, lng: 138.252924 },
-      zoom: 4,
-    });
-  } else {
-    const lat = parseFloat(mapDisplay.dataset.latitude);
-    const lng = parseFloat(mapDisplay.dataset.longitude);
-    map = new google.maps.Map(mapDisplay, {
-      center: { lat: lat, lng: lng },
-      zoom: 12,
-    });
-    const marker = new google.maps.Marker({
-      map: map,
-      position: { lat: lat, lng: lng },
-    });
-  }
-  return map;
-}
-
-function codeAddress() {
-  let map = this.map;
-  let geocoder = this.geocoder;
-  let lat = document.getElementById("lat");
-  let lng = document.getElementById("lng");
-
-  let inputAddress = document.getElementById("address").value;
-  geocoder.geocode({ address: inputAddress }, function (results, status) {
-    if (status == "OK") {
-      map.setCenter(results[0].geometry.location);
-      const marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location,
-      });
-      lat.value = results[0].geometry.location.lat();
-      lng.value = results[0].geometry.location.lng();
-    } else {
-      alert("該当する結果がありませんでした：" + status);
-    }
-  });
-}
