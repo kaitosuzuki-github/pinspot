e4572267dd58cd27c65d67c148990964ee2907d3
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-08 16:41:39 +0900

    fix: #109 投稿機能でバリデーションエラーがあったとき、jsが効かないため修正

--- app/javascript/custom/image_preview.js
+++ app/javascript/custom/image_preview.js
@@ -1,15 +1,14 @@
-document.addEventListener(
-  "turbo:load",
-  function () {
-    const fileInput = document.getElementById("fileInput");
-    if (fileInput != null) {
-      fileInput.addEventListener("change", imagePreview, false);
-    }
-  },
-  false
-);
+document.addEventListener("DOMContentLoaded", imagePreview, false);
+document.addEventListener("turbo:render", imagePreview, false);
 
-function imagePreview(e) {
+function imagePreview() {
+  const fileInput = document.getElementById("fileInput");
+  if (fileInput != null) {
+    fileInput.addEventListener("change", setImage, false);
+  }
+}
+
+function setImage(e) {
   let image = document.getElementById("image");
   const file = e.currentTarget.files[0];
   image.setAttribute("src", URL.createObjectURL(file));
