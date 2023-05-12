0882dcf04c6c4f60c55c680fcdbd92157e851494
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-12 16:20:12 +0900

    feat: #119 userモデルの新規作成と同時にprofileモデルも新規作成されるように実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :set_search
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_search
     @q = Post.ransack(params[:q])
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:name]])
   end
 end
