49ae4c6384afc3dddfc707ddc15d4d518a65cb27
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-13 18:51:49 +0900

    feat: #117 プロフィール機能の読み出し処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'profiles/:id', to: 'profiles#show', as: 'profile'
   root "home#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
