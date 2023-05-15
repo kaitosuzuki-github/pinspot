141c395bb0332ef5633efad224783020cfe0cd2b
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-15 22:50:33 +0900

    feat: #18 プロフィール機能のバリデーションの追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,5 +3,6 @@ class Profile < ApplicationRecord
   has_one_attached :cover
   has_one_attached :avatar
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :introduction, length: { maximum: 1000 }
 end
