d2e59da0a9509de0fa23d867ae1835f1a4106cd6
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-15 22:42:11 +0900

    feat: #18 プレビュー処理がプロフィール機能と投稿機能の両方に対応できるよう修正

--- app/javascript/custom/image_preview.js
+++ app/javascript/custom/image_preview.js
@@ -2,14 +2,24 @@ document.addEventListener("DOMContentLoaded", imagePreview, false);
 document.addEventListener("turbo:render", imagePreview, false);
 
 function imagePreview() {
-  const fileInput = document.getElementById("fileInput");
+  let fileInput = Array.from(document.querySelectorAll('input[type="file"]'));
   if (fileInput != null) {
-    fileInput.addEventListener("change", setImage, false);
+    fileInput.forEach(function (element) {
+      const fileInputId = element.getAttribute("id");
+      const imageId = fileInputId + "_preview";
+      element.addEventListener(
+        "change",
+        { imageId: imageId, handleEvent: setImage },
+        false
+      );
+    });
   }
 }
 
 function setImage(e) {
-  let image = document.getElementById("image");
+  const image = document.getElementById(this.imageId);
   const file = e.currentTarget.files[0];
-  image.setAttribute("src", URL.createObjectURL(file));
+  if (file.type.indexOf("image") == 0) {
+    image.setAttribute("src", URL.createObjectURL(file));
+  }
 }
