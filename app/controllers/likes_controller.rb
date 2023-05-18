805541dcf14f9a565c0c9aa5c0e9ae76e21e09d0
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-18 17:44:44 +0900

    feat: #141 limit_user_likesメソッドを不要なため削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,5 @@
 class LikesController < ApplicationController
-  before_action :limit_user_likes
+  before_action :authenticate_user!
 
   def create
     like = Like.new(user_id: current_user.id, post_id: params[:post_id])
@@ -11,14 +11,5 @@ class LikesController < ApplicationController
     like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     like.destroy
     redirect_to post_path(params[:post_id])
-  end
-
-  private
-
-  def limit_user_likes
-    post = Post.find(params[:post_id])
-    if post.user_id == current_user.id
-      redirect_to post_path(params[:post_id])
-    end
   end
 end
