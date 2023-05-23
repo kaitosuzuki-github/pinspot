7f15b3dbe8c0e93748d4923b66218d2f74ddbef9
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-05-23 18:21:57 +0900

    feat: #101 ドロップダウンメニューの実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "custom/google_map";
-import "custom/image_preview";
+import "customs/google_map";
+import "customs/image_preview";
+import "customs/dropdown";
