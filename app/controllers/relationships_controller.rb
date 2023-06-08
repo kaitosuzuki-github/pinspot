fcb7aa8bdae041787ab173fe95bff53cece5470d
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-06-08 22:15:04 +0900

    fix: #170 コントローラのアクションのアクセス制限の修正とそれに伴う変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,22 +1,24 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  before_action :limit_self_user!
+  before_action :limit_user
 
   def create
     unless current_user.following?(params[:user_id])
       current_user.follow(params[:user_id])
-      redirect_back(fallback_location: root_path)
     end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
+    if current_user.following?(params[:user_id])
+      current_user.unfollow(params[:user_id])
+    end
     redirect_back(fallback_location: root_path)
   end
 
   private
 
-  def limit_self_user!
+  def limit_user
     if current_user.same_user?(params[:user_id])
       redirect_back(fallback_location: root_path)
     end
