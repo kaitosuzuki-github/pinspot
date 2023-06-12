288f5ceb448cb46ccc7bba02199388dcd4bb6c9a
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-06-12 13:01:39 +0900

    fix: #199 トップページのgooglemapのマーカーのリンクでエラーが出ていたため修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     posts = Post.all
-    @posts_json = posts.to_json(only: [:title, :location, :latitude, :longitude])
+    @posts_json = posts.to_json(only: [:id, :title, :location, :latitude, :longitude])
     @new_posts = Post.with_attached_image.order(created_at: :DESC).limit(4)
   end
 end
