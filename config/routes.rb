d8416b9171fa21182bdf14b786f4499375aef7dd
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-06-12 21:54:38 +0900

    refactor: #158 ルーティングのリファクタリングとそれに伴う修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
-  resources :contacts, only: [:new, :create]
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    post 'users/guest_sign_in', to:'users/sessions#guest_sign_in'
+    get 'users/show', to:'users/registrations#show'
+  end
   resources :users, only: [] do
     resource :relationships, only: [:create, :destroy]
   end
@@ -20,9 +24,5 @@ Rails.application.routes.draw do
       get "search"
     end
   end
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  devise_scope :user do
-    post 'users/guest_sign_in', to:'users/sessions#guest_sign_in'
-  end
-  get "users/show"
+  resources :contacts, only: [:new, :create]
 end
