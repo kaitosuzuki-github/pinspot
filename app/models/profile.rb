790e3baca522fceb5d17943b0797c0adde1b635e
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-06-16 12:12:53 +0900

    fix: #55 不要なバリデーションがあったため、修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,10 +3,10 @@ class Profile < ApplicationRecord
   has_one_attached :cover
   has_one_attached :avatar
 
-  validates :cover, attached: true, content_type: ['image/png', 'image/jpeg'],
+  validates :cover, content_type: ['image/png', 'image/jpeg'],
                     size: { less_than: 15.megabytes },
                     dimension: { width: { min: 400 }, height: { min: 400 } }
-  validates :avatar, attached: true, content_type: ['image/png', 'image/jpeg'],
+  validates :avatar, content_type: ['image/png', 'image/jpeg'],
                      size: { less_than: 15.megabytes }
   validates :name, presence: true, length: { maximum: 100 }
   validates :introduction, length: { maximum: 1000 }
