9a994d5c8e3c099a044f88925663843f5fa111b4
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-06-22 18:22:52 +0900

    refactor: #245 処理が冗長であったため、ヘルパーメソッドとして定義

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,4 +7,36 @@ module PostsHelper
       post.image.variant(resize_to_limit: [1000, 1000])
     end
   end
+
+  def display_like_button(post_id)
+    if user_signed_in?
+      if current_user.like?(post_id)
+        button_to post_likes_path(post_id), method: :delete do
+          render "shared/icons/heart", class_list: "w-8 h-8 fill-current stroke-current"
+        end
+      else
+        button_to post_likes_path(post_id), method: :post do
+          render "shared/icons/heart", class_list: "w-8 h-8 fill-none stroke-current"
+        end
+      end
+    else
+      render "shared/icons/heart", class_list: "w-8 h-8 fill-none stroke-current"
+    end
+  end
+
+  def display_bookmark_button(post_id)
+    if user_signed_in?
+      if current_user.bookmarking?(post_id)
+        button_to post_bookmarks_path(post_id), method: :delete do
+          render "shared/icons/bookmark", class_list: "w-8 h-8 fill-current stroke-current"
+        end
+      else
+        button_to post_bookmarks_path(post_id), method: :post do
+          render "shared/icons/bookmark", class_list: "w-8 h-8 fill-none stroke-current"
+        end
+      end
+    else
+      render "shared/icons/bookmark", class_list: "w-8 h-8 fill-none stroke-current"
+    end
+  end
 end
