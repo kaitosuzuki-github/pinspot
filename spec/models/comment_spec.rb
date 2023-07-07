475d37e0a795b283a1c4fd6a372a4f1e429f3d4a
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-07 18:55:20 +0900

    test: #366 commetモデルのバリデーションのテストを行うため

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe 'validation' do
+    context 'contentの文字数が255文字以下の場合' do
+      let(:comment) { build(:comment, content: 'a' * 255) }
+
+      it '有効であること' do
+        expect(comment).to be_valid
+      end
+    end
+
+    context 'contentの文字数が255文字より多い場合' do
+      let(:comment) { build(:comment, content: 'a' * 256) }
+
+      it '無効であること' do
+        expect(comment).to be_invalid
+      end
+    end
+  end
 end
