09d9e29cae0639b8bab56b74e2a88263c8693cba
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-11 19:48:35 +0900

    test: #459 let(:category)の処理が2回あり、冗長であったため修正

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'validation' do
-    context 'nameの文字数が100文字以下の場合' do
-      let(:category) { build(:category, name: 'a' * 100) }
+    let(:category) { build(:category) }
 
+    context 'nameの文字数が100文字以下の場合' do
       it '有効であること' do
+        category.name = 'a' * 100
         expect(category).to be_valid
       end
     end
 
     context 'nameの文字数が100文字より多い場合' do
-      let(:category) { build(:category, name: 'a' * 101) }
-
       it '無効であること' do
+        category.name = 'a' * 101
         expect(category).to be_invalid
       end
     end
