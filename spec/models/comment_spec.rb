7970ca9634a4f47b31d5d4e65fc0721ac328bc67
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-11 19:58:17 +0900

    test: #461 let(:comment)の処理が2回あり、冗長であったため修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'validation' do
-    context 'contentの文字数が255文字以下の場合' do
-      let(:comment) { build(:comment, content: 'a' * 255) }
+    let(:comment) { build(:comment) }
 
+    context 'contentの文字数が255文字以下の場合' do
       it '有効であること' do
+        comment.content = 'a' * 255
         expect(comment).to be_valid
       end
     end
 
     context 'contentの文字数が255文字より多い場合' do
-      let(:comment) { build(:comment, content: 'a' * 256) }
-
       it '無効であること' do
+        comment.content = 'a' * 256
         expect(comment).to be_invalid
       end
     end
