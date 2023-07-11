aa2cce4d7f923ce09b24f5ac56df79ce51ddf713
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-11 19:50:45 +0900

    test: #459 itの説明が分かりづらかったため修正

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Category, type: :model do
     let(:category) { build(:category) }
 
     context 'nameの文字数が100文字以下の場合' do
-      it '有効であること' do
+      it '許可すること' do
         category.name = 'a' * 100
         expect(category).to be_valid
       end
     end
 
     context 'nameの文字数が100文字より多い場合' do
-      it '無効であること' do
+      it '許可しないこと' do
         category.name = 'a' * 101
         expect(category).to be_invalid
       end
