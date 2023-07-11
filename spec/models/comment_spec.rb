1fe9cddd81b9cf6da8fb51ad869e0cf6a37bf5f8
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-11 19:59:57 +0900

    test: #461 itの説明が分かりづらかったため修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Comment, type: :model do
     let(:comment) { build(:comment) }
 
     context 'contentの文字数が255文字以下の場合' do
-      it '有効であること' do
+      it '許可すること' do
         comment.content = 'a' * 255
         expect(comment).to be_valid
       end
     end
 
     context 'contentの文字数が255文字より多い場合' do
-      it '無効であること' do
+      it '許可しないこと' do
         comment.content = 'a' * 256
         expect(comment).to be_invalid
       end
