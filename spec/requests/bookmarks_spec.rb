33c821af00fda93e750cfedd6341aa8eaed1e5b8
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-12 20:27:57 +0900

    test: #381 bookmarksコントローラのdestroyアクションに対する、request specの実装

--- spec/requests/bookmarks_spec.rb
+++ spec/requests/bookmarks_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Bookmarks", type: :request do
-  describe 'POST /posts/:post_id/bookmarks' do
-    let(:user) { create(:user) }
-    let(:bookmark_post) { create(:post) }
+  let(:user) { create(:user) }
+  let(:bookmark_post) { create(:post) }
 
+  describe 'POST /posts/:post_id/bookmarks' do
     context '正常な場合' do
       before do
         sign_in user
@@ -44,6 +44,60 @@ RSpec.describe "Bookmarks", type: :request do
 
       it 'トップページにリダイレクトすること' do
         post post_bookmarks_path(bookmark_post.id)
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe 'DELETE /posts/:post_id/bookmarks' do
+    context '正常な場合' do
+      before do
+        sign_in user
+        user.bookmarks.create(post_id: bookmark_post.id)
+      end
+
+      it 'レスポンスコード302が返ってくること' do
+        delete post_bookmarks_path(bookmark_post.id)
+        expect(response).to have_http_status(302)
+      end
+
+      it 'ユーザーに関連するブックマークが削除されること' do
+        expect { delete post_bookmarks_path(bookmark_post.id) }.to change { user.bookmarks.count }.by(-1)
+      end
+
+      it 'トップページにリダイレクトされること' do
+        delete post_bookmarks_path(bookmark_post.id)
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'サインインせずに、ブックマークを削除した場合' do
+      before do
+        user.bookmarks.create(post_id: bookmark_post.id)
+        delete post_bookmarks_path(bookmark_post.id)
+      end
+
+      it 'レスポンスコード302が返ってくること' do
+        expect(response).to have_http_status(302)
+      end
+
+      it 'サインインページへリダイレクトされること' do
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ユーザーがブックマークしていなかった場合' do
+      before do
+        sign_in user
+        user.bookmarks.create(post_id: bookmark_post.id)
+        delete post_bookmarks_path(bookmark_post.id)
+      end
+
+      it 'レスポンスコード302が返ってくること' do
+        expect(response).to have_http_status(302)
+      end
+
+      it 'トップページへリダイレクトされること' do
         expect(response).to redirect_to root_path
       end
     end
