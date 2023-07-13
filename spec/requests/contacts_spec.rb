8311873844d9ce2720e171083d1fc0ca61d83c7e
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-13 20:42:19 +0900

    test: #384 contactsコントローラのnewアクションに対する、request specの実装

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Contacts", type: :request do
+  describe 'GET /contacts/new' do
+    before do
+      get new_contact_path
+    end
+
+    it '正常なレスポンスを返すこと' do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
