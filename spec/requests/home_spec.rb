10085f98fb9c7f57117b402300e3796b5daa058f
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-14 16:30:29 +0900

    test: #386 homeコントローラのindexアクションに対する、request specの実装

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
+  describe 'GET /' do
+    let!(:post) { create(:post) }
+    before do
+      get root_path
+    end
+
+    it '正常なレスポンスを返すこと' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it '投稿名を表示すること' do
+      expect(response.body).to include post.title
+    end
+  end
 end
