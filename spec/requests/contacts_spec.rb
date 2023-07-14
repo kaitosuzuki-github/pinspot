69ffedce2e2b756382c1aba711fd963a36a7453f
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-14 14:05:07 +0900

    test: #385 contactsコントローラのcreateアクションに対する、request specの実装

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -10,4 +10,73 @@ RSpec.describe "Contacts", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'POST /contacts' do
+    subject do
+      post contacts_path, :params => params
+    end
+
+    context '正常な場合' do
+      let(:params) do
+        { :contact => { :email => Faker::Internet.email, :subject => Faker::Lorem.sentence, :message => Faker::Lorem.paragraph } }
+      end
+
+      it 'レスポンスコード302を返すこと' do
+        subject
+        expect(response).to have_http_status(302)
+      end
+
+      it 'contactを作成すること' do
+        expect { subject }.to change { Contact.count }.by(1)
+      end
+
+      it 'お問い合わせメールを送ること' do
+        expect { subject }.to change { ActionMailer::Base.deliveries.size }.by(1)
+      end
+
+      it 'flash[:notice]で「お問い合わせを受け付けました」を表示すること' do
+        subject
+        expect(flash[:notice]).to eq 'お問い合わせを受け付けました'
+      end
+
+      it 'トップページへリダイレクトすること' do
+        subject
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'パラメータが不正でcontactを保存できなかった場合' do
+      let(:params) do
+        { :contact => { :email => nil, :subject => nil, :message => nil } }
+      end
+
+      it 'レスポンスコード422を返すこと' do
+        subject
+        expect(response).to have_http_status(422)
+      end
+
+      it 'contactを作成しないこと' do
+        expect { subject }.to change { Contact.count }.by(0)
+      end
+
+      it 'お問い合わせメールを送らないこと' do
+        expect { subject }.to change { ActionMailer::Base.deliveries.size }.by(0)
+      end
+
+      it 'エラーを表示すること' do
+        subject
+        expect(response.body).to include 'エラー'
+      end
+    end
+
+    context 'パラメータのハッシュにcontactがない場合' do
+      let(:params) do
+        { :test => { :email => nil, :subject => nil, :message => nil } }
+      end
+
+      it 'パラメータのエラーが発生すること' do
+        expect { subject }.to raise_error(ActionController::ParameterMissing)
+      end
+    end
+  end
 end
