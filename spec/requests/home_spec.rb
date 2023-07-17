438642daf22fe0863cf623790bf267192d119aed
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-17 20:12:25 +0900

    test: #500 コードの可読性が落ちるため、rspecのbeforeブロックでhttpリクエストを実装せず、example内で実装

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 RSpec.describe "Homes", type: :request do
   describe 'GET /' do
     let!(:post) { create(:post) }
-    before do
-      get root_path
-    end
 
     it '正常なレスポンスを返すこと' do
+      get root_path
       expect(response).to have_http_status(:success)
     end
 
     it '投稿名を表示すること' do
+      get root_path
       expect(response.body).to include post.title
     end
   end
