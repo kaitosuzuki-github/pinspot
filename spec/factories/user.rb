a272d860061b5e40155fe7fb06c3203717a836ef
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-17 19:23:59 +0900

    test: #493 userモデルにprofileのバリデーションを追加したことによるrspecの修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 6) }
     confirmed_at { Time.now }
+
+    after(:build) do |user|
+      user.build_profile
+      user.profile.name = Faker::Internet.username
+    end
   end
 end
