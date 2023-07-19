9bd0a02630811d4801f0bf2b5342ccb4797f403b
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-19 19:36:47 +0900

    test: #398 profilesコントローラのupdateアクションに対しての、request specの実装

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -70,4 +70,87 @@ RSpec.describe "Profiles", type: :request do
       end
     end
   end
+
+  describe 'PUT /profiles/:id' do
+    let(:user) { create(:user) }
+
+    context '正常な場合' do
+      before do
+        sign_in user
+      end
+
+      it 'レスポンスコード302を返すこと' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile) }
+        expect(response).to have_http_status(302)
+      end
+
+      it 'ログインしているユーザーに関連するプロフィール名を編集すること' do
+        expect do
+          put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile, name: 'test') }
+        end .to change { Profile.last.name }.from(user.profile.name).to('test')
+      end
+
+      it '「変更しました」を表示すること' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile) }
+        expect(flash[:notice]).to include '変更しました'
+      end
+
+      it '編集したプロフィールページへリダイレクトすること' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile) }
+        expect(response).to redirect_to user.profile
+      end
+    end
+
+    context 'サインインしていない場合' do
+      it 'レスポンスコード302を返すこと' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile) }
+        expect(response).to have_http_status(302)
+      end
+
+      it 'サインインページへリダイレクトすること' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile) }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ログインしているユーザーとプロフィールのユーザーが同じではない場合' do
+      let(:other_user) { create(:user) }
+
+      before do
+        sign_in other_user
+      end
+
+      it 'レスポンスコード302を返すこと' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile) }
+        expect(response).to have_http_status(302)
+      end
+
+      it 'トップページへリダイレクトすること' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile) }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      before do
+        sign_in user
+      end
+
+      it 'レスポンスコード422を返すこと' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile, name: nil) }
+        expect(response).to have_http_status(422)
+      end
+
+      it 'ログインしているユーザーに関連するプロフィール名を編集しないこと' do
+        expect do
+          put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile, name: nil) }
+        end .to_not change { Profile.last.name }
+      end
+
+      it '「エラー」を表示すること' do
+        put profile_path(user.profile.id), :params => { :profile => attributes_for(:profile, name: nil) }
+        expect(response.body).to include 'エラー'
+      end
+    end
+  end
 end
