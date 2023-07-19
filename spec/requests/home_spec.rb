a4db3ce39a2d976ab518cca3ff8c0b65e9a9997d
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-07-19 14:30:28 +0900

    test: #516 homeコントローラのindexアクションのテストで、すべての投稿名が表示されるか確認するexampleに修正

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe 'GET /' do
-    let!(:post) { create(:post) }
+    let!(:posts) { create_list(:post, 2) }
 
     it '正常なレスポンスを返すこと' do
       get root_path
       expect(response).to have_http_status(:success)
     end
 
-    it '投稿名を表示すること' do
+    it 'すべての投稿名を表示すること' do
       get root_path
-      expect(response.body).to include post.title
+      expect(response.body).to include posts[0].title
+      expect(response.body).to include posts[1].title
     end
   end
 end
