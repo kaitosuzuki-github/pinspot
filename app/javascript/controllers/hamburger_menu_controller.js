b070cdb5540172b03abb326acc65de2b44d420b3
Kaito Suzuki <kaitosuzuki.info@gmail.com>
2023-08-15 16:09:20 +0900

    fix: #575 hamburger_menuが上手く動かないことがあったため、stimulusを使って実装するように変更

---
+++ app/javascript/controllers/hamburger_menu_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["menu", "toggle"];
+
+  toggle() {
+    this.menuTarget.classList.toggle("hidden");
+    this.toggleTargets[0].classList.toggle("hidden");
+    this.toggleTargets[1].classList.toggle("hidden");
+  }
+
+  hide(event) {
+    if (
+      !this.element.contains(event.target) &&
+      !this.menuTarget.classList.contains("hidden")
+    ) {
+      this.menuTarget.classList.add("hidden");
+      this.toggleTargets[0].classList.toggle("hidden");
+      this.toggleTargets[1].classList.toggle("hidden");
+    }
+  }
+}
